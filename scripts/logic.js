f4fd18a78ab868944fee1ddbe5b9ce2e674a32a0
yunish122 <manandharunish1@gmail.com>
2025-07-09 00:56:15 +0545

    main logics and css done, will need imporvements

---
+++ scripts/logic.js
@@ -0,0 +1,145 @@
+
+
+let currMode = {}
+
+const modes = {
+    focus : {
+        mode: 'focus',
+        intervalId: null,
+        isRunning: false,
+        time_left: 25*60,
+        actual_left: 25*60,
+        elapsed_time:0
+    },
+    thirty_min:{
+        mode: "30min",
+        intervalId: null,
+        isRunning: false,
+        time_left: 30*60,
+        actual_left: 30*60,
+
+        elapsed_time:0
+    },
+    fifty_min:{
+        mode: "50min",
+        intervalId: null,
+        isRunning: false,
+        time_left: 50*60,
+        actual_left: 50*60,
+
+        elapsed_time:0
+    },
+    short_rest: {
+        mode: "shortRest",
+        intervalId: null,
+        isRunning: false,
+        time_left: 5*60,
+        actual_left: 5*60,
+
+        elapsed_time:0
+    },
+    long_rest:{
+        mode: "longRest",
+        intervalId: null,
+        isRunning: false,
+        time_left: 15*60,
+        actual_left: 15*60,
+
+        elapsed_time:0
+    }
+}
+
+
+// document.getElementById('25').addEventListener("click", (e)=>{
+//     if(state_mgt.isRunning === true){
+//         return;
+//     }
+//     state_mgt.intervalId = setInterval(()=>{
+//         start();
+//         if(state_mgt.elapsed_time >= 1500){
+//             return
+//         }
+//     },1000)
+
+// })
+
+// switch garxa mode
+function switchMode(mode){
+    currMode = modes[mode];
+    uiUpdate()
+}
+
+function uiUpdate(){
+
+    let minutes =  Math.floor( currMode.time_left / 60);
+
+    let seconds = currMode.time_left%60;
+
+    let mm = String(minutes).padStart(2,"0");
+    let ss = String(seconds).padStart(2,"0")
+    document.getElementById("timer").textContent = `${mm}:${ss}`
+
+}
+
+function start(){
+    if(currMode.isRunning){
+        return;
+    }
+    currMode.isRunning = true;
+    let id = setInterval(()=>{
+        currMode.time_left--;
+        currMode.elapsed_time++;
+        currMode.intervalId = id;
+        uiUpdate()    
+    },1000)
+
+}
+
+function pause(){
+    if(!currMode.isRunning){
+        return
+    }
+
+    clearInterval(currMode.intervalId);
+    currMode.isRunning = false;
+}
+
+function reset(){
+    currMode.isRunning = false;
+    currMode.time_left = 25 * 60;
+    currMode.elapsed_time = 0;
+    clearInterval(currMode.intervalId);
+    document.getElementById("timer").textContent = `${currMode.actual_left/60}:00`;
+
+}
+
+
+//event listeners 
+
+document.getElementById('30min').addEventListener('click',()=>{
+    switchMode('thirty_min');
+
+})
+document.getElementById('50min').addEventListener('click',()=>{
+    switchMode('fifty_min');
+
+})
+document.getElementById('25min').addEventListener('click',()=>{
+    switchMode('focus');
+
+})
+
+document.getElementById('short_rest').addEventListener('click',()=>{
+    switchMode('short_rest');
+
+})
+document.getElementById('long_rest').addEventListener('click',()=>{
+    switchMode('long_rest');
+
+})
+
+document.getElementById('start').addEventListener('click',()=>start());
+document.getElementById('pause').addEventListener('click',()=>pause());
+document.getElementById('reset').addEventListener('click',()=>reset());
+
+switchMode('focus');
